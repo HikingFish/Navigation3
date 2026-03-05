b0019b697e066026a4d797c31838274393afe539
HikingFish <cheongwaikian1@gmail.com>
2026-03-05 15:07:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/navigation3/navigation/NavigationRoot.kt
@@ -0,0 +1,43 @@
+package com.example.navigation3.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
+import androidx.navigation3.ui.NavDisplay
+import com.example.navigation3.screens.TodoDetailScreen
+import com.example.navigation3.screens.TodoListScreen
+
+@Composable
+fun NavigationRoot(
+    modifier: Modifier = Modifier
+){
+    val backStack = rememberNavBackStack(
+        Route.TodoList
+    )
+    NavDisplay(
+        modifier = modifier,
+        backStack = backStack,
+        entryDecorators = listOf(
+            rememberSaveableStateHolderNavEntryDecorator(),
+            rememberViewModelStoreNavEntryDecorator()
+        ),
+        entryProvider = entryProvider {
+            entry<Route.TodoList> {
+                TodoListScreen(
+                    onTodoClick = {
+                        backStack.add(Route.TodoDetail(it))
+                    }
+                )
+            }
+            entry<Route.TodoDetail>{
+                TodoDetailScreen(
+                    todo = it.todo
+                )
+            }
+        }
+    )
+}
---
+++ app/src/main/java/com/example/navigation3/viewmodels/TodoListViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.navigation3.viewmodels
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class TodoListViewModel: ViewModel() {
+
+    private val _todos = MutableStateFlow((1 .. 100).map {"Todo $it"}
+    )
+    val todos = _todos.asStateFlow()
+}
