9f117e90de0ed8ff0e08198db33f00923b94e73e
HikingFish <cheongwaikian1@gmail.com>
2026-03-12 09:18:02 +0800

    Completed the nav root display

--- app/src/main/java/com/example/navigation3/navigation/NavigationRoot.kt
+++ app/src/main/java/com/example/navigation3/navigation/NavigationRoot.kt
@@ -1,7 +1,15 @@
 package com.example.navigation3.navigation
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.entryProvider
@@ -18,15 +26,57 @@ fun NavigationRoot(
     val backStack = rememberNavBackStack(
         Route.TodoList
     )
-    NavDisplay(
+    Scaffold (
         modifier = modifier,
-        backStack = backStack,
-        entryDecorators = listOf(
-            rememberSaveableStateHolderNavEntryDecorator(),
-            rememberViewModelStoreNavEntryDecorator()
-        ),
-        entryProvider = entryProvider {
-
+        bottomBar = {
+            TodoNavigationBar(
+                selectedKey = Route.TodoList,
+                onSelectKey = {
+                    // TODO: Select key
+                }
+            )
         }
-    )
+    ) { innerPadding ->
+            NavDisplay(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                backStack = backStack,
+                entryDecorators = listOf(
+                    rememberSaveableStateHolderNavEntryDecorator(),
+                    rememberViewModelStoreNavEntryDecorator()
+                ),
+                entryProvider = entryProvider {
+                    entry<Route.TodoList> {
+                        TodoListScreen(
+                            onTodoClick = {
+                                backStack.add(Route.TodoDetail(it))
+                            }
+                        )
+                    }
+                    entry<Route.TodoFavorite> {
+                        TodoListScreen(
+                            onTodoClick = {
+                                backStack.add(Route.TodoDetail(it))
+                            }
+                        )
+                    }
+                    entry<Route.TodoDetail> {
+                        TodoDetailScreen(
+                            todo = it.todo
+                        )
+                    }
+                    entry<Route.Settings> {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text("Settings")
+                        }
+                    }
+                }
+            )
+    }
+
 }
